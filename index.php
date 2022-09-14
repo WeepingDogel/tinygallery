51c3c6b902090efe803f947f628d2ccf87700cff
WeepingDogel <weepingdogel@gmail.com>
2022-09-14 14:24:35 +0800

    Use CSS and write some elements.

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <html>
     <head>
         <title>HomePage</title>
+        <link href="css/MianStyle.css" type="text/css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-<?php
-echo "Hello World!";
-?>
+        <div class="top">
+            <h1>TinyGallery</h1>
+            <h2>HomePage</h2>
+        </div>
     </body>
 </html>
