a9ab05da8e1ce322073b2e7e6960d36e34500e2d
WeepingDogel <weepingdogel@gmail.com>
2022-11-29 15:39:28 +0800

    Add js, but it's useless at present.

--- js/Main.js
+++ js/Main.js
@@ -3,3 +3,12 @@
 function Hi(){
     alert("Under Development.");
 }
+
+function Switch_Form(form_ID1, form_ID2){
+    var ElementToDisplay = document.getElementById(form_ID1);
+    var ElementToHide = document.getElementById(form_ID2);
+    ElementToDisplay.style.display = "block";
+    ElementToHide.style.display = "none";
+    console.log("User changed the form mode.");
+    return 0;
+}
