f0d7217fa329e385a590a9a8311d146bf1deba7f
WeepingDogel <weepingdogel@gmail.com>
2022-12-22 19:48:44 +0800

    Start

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,23 +3,36 @@
 
 import sqlite3
 import click
-from flask import *
+from flask import current_app, g
 
-def StablizeDATABASE(name, sql):
-    database = sqlite3.connect(name)
-    database.execute(sql)
-    database.commit()
-    database.close()
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
 
+    return g.db
 
-@click.command("init-db")
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('createDATA.sql') as f:
+        db.executescript(f.read().decode('utf-8'))
+
+@click.command('init-db')
 def init_db_command():
-    with open("createDATA.sql") as sqlFile:
-        StablizeDATABASE("database.db", sqlFile.read())
-        click.echo("DATABASE INITIALIZED!")
-
-bp = Blueprint('db', __name__, "/")
+    init_db()
+    click.echo("Initialized the database.")
 
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
