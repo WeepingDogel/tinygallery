155fc13815ef3cb534dda8ad75009de88667a2c0
WeepingDogel <weepingdogel@gmail.com>
2022-12-23 01:27:21 +0800

    Finish

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,28 +8,31 @@ from . import db
 from . import auth
 from flask import *
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = 'dev',
         DATABASE = os.path.join(app.instance_path, 'database.sqlite')
     )
-    # if test_config is None:
-    #     app.config.from_pyfile('config.py', silent=True)
-    # else:
-    #     app.config.from_mapping(test_config)
-
+    
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+    
     @app.route("/")
     def index():
         return render_template("index.html", PageTitle="HomePage", userAvaterImage="static/avatars/test.jpg", userName="Please Log In", logIN_Display="block" ,logOUT_Display="none")
+    
     @app.route("/login_and_register")
     def LoginPage():
         return render_template("auth.html", PageTitle="Sign Up or Sign In.")
     
     db.init_app(app)
+    app.register_blueprint(auth.bp)
 
     return app
