e8dd6892deaee71dd5720b51a6e3dacecce91b6f
WeepingDogel <weepingdogel@gmail.com>
2023-01-05 14:39:43 +0800

    Remark.

--- flaskr/remark.py
+++ flaskr/remark.py
@@ -1,6 +1,6 @@
 # Full Image Viewing And Remarking.
 
-import uuid
+import uuid,time
 from flaskr.db import get_db
 from flask import(
     Blueprint,
@@ -22,6 +22,11 @@ def ViewFullImage(IMAGE_UUID):
             'SELECT FileName,ImageTitle FROM IMAGES WHERE UUID = ?;',
             (IMAGE_UUID,)
         ).fetchone()
+        Comments = db.execute(
+            "SELECT * FROM COMMENTS WHERE postUUID = ? Order By Date DESC",
+            (IMAGE_UUID,)
+            )
+
     if 'username' in session:
         userAvaterImage = current_app.config['PUBLIC_USERFILES'] + '/' + session['username'] + '/avatar.jpg'
         return render_template(
@@ -29,9 +34,33 @@ def ViewFullImage(IMAGE_UUID):
             PageTitle=Data['ImageTitle'], 
             FileName=Data['FileName'],
             userAvaterImage=userAvaterImage,
-            userName=session['username'])
+            userName=session['username'],
+            postUUID=IMAGE_UUID,
+            Comments=Comments)
     else:
         return render_template(
             "remark.html", 
             PageTitle=Data['ImageTitle'], 
-            FileName=Data['FileName'])
+            FileName=Data['FileName'],
+            postUUID=IMAGE_UUID,
+            Comments=Comments)
+
+@bp.route("/comment", methods=('POST','GET'))
+def SendComment():
+    if request.method == 'POST':
+        db = get_db()
+        User = session['username']
+        postUUID = request.form['postUUID']
+        Content = request.form['ContentOfComments']
+        remarkUUID = str(uuid.uuid4())
+        Date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        if Content == "":
+            return redirect(url_for("remark.ViewFullImage", IMAGE_UUID=postUUID))
+        else:
+            db.execute(
+                "INSERT INTO COMMENTS(postUUID, remarkUUID, User, Comment, Date) VALUES(?,?,?,?,?)",
+                (postUUID, remarkUUID, User, Content, Date))
+            db.commit()
+            return redirect(url_for("remark.ViewFullImage", IMAGE_UUID=postUUID))
+        
+        
